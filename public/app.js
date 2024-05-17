bfcc0be283605fc9b6c6ad357c53ed8c9c70afca
Nauffal Rizky <sazane2005@gmail.com>
2024-05-17 09:58:46 +0700

    MASSIVECHANGES170524

--- public/app.js
+++
@@ -1,5 +0,0 @@
-import './scripts/components/navbar.js';
-import './scripts/components/jumbotron.js';
-import './scripts/components/about-center.js';
-import main from './scripts/view/main.js';
-document.addEventListener('DOMContentLoaded', main);
