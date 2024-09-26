8fa83101069f5a2d8d76f11ec28803153c2192ab
nauffal-rizky <nauffal2005@gmail.com>
2024-09-26 10:29:24 +0700

    finally

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const nav = document.querySelector('.navbar');
+const nav = document.querySelector(".navbar");
 
-const goToUp = document.querySelector('.go-to-up');
-window.addEventListener('scroll', () => {
+const goToUp = document.querySelector(".go-to-up");
+window.addEventListener("scroll", () => {
   const scrollHeight = window.pageYOffset;
 
   if (scrollHeight > 0) {
-    nav.classList.add('scrolled');
+    nav.classList.add("scrolled");
   } else {
-    nav.classList.remove('scrolled');
+    nav.classList.remove("scrolled");
   }
 
   if (scrollHeight > 500) {
-    goToUp.classList.add('show');
+    goToUp.classList.add("show");
   } else {
-    goToUp.classList.remove('show');
+    goToUp.classList.remove("show");
   }
 });
 
@@ -24,7 +24,7 @@ if (window.innerWidth <= 768 && window.innerWidth > 576) {
 } else if (window.innerWidth <= 576) {
   nav.style.height = `${initialHeight - 12}px`;
 }
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   if (window.innerWidth <= 768 && window.innerWidth > 576) {
     nav.style.height = `${initialHeight}px`;
   } else if (window.innerWidth <= 576) {
@@ -32,27 +32,27 @@ window.addEventListener('resize', () => {
   }
 });
 
-const navDrop = document.querySelector('.logo i');
-navDrop.addEventListener('click', () => {
-  if (!nav.classList.contains('drop')) {
-    nav.classList.add('drop');
+const navDrop = document.querySelector(".logo i");
+navDrop.addEventListener("click", () => {
+  if (!nav.classList.contains("drop")) {
+    nav.classList.add("drop");
   } else {
-    nav.classList.remove('drop');
+    nav.classList.remove("drop");
   }
 });
 
-const toggles = document.querySelectorAll('.toggle-links li a');
+const toggles = document.querySelectorAll(".toggle-links li a");
 toggles.forEach((toggle) => {
-  toggle.addEventListener('click', (e) => {
+  toggle.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const id = e.currentTarget.getAttribute('href').slice(1);
+    const id = e.currentTarget.getAttribute("href").slice(1);
     const targetElement = document.getElementById(id);
 
     const navHeight = nav.getBoundingClientRect().height;
     let targetPosition = targetElement.offsetTop - navHeight;
 
-    if (nav.classList.contains('drop')) {
+    if (nav.classList.contains("drop")) {
       if (window.innerWidth <= 768 && window.innerWidth > 576) {
         targetPosition += 278;
       } else if (window.innerWidth <= 576) {
@@ -60,7 +60,7 @@ toggles.forEach((toggle) => {
       }
     }
 
-    if (nav.classList.contains('scrolled')) {
+    if (nav.classList.contains("scrolled")) {
       if (window.innerWidth <= 768 && window.innerWidth > 576) {
         targetPosition -= initialHeight;
       } else if (window.innerWidth <= 576) {
@@ -79,7 +79,7 @@ toggles.forEach((toggle) => {
       top: targetPosition,
     });
 
-    nav.classList.remove('drop');
+    nav.classList.remove("drop");
   });
 });
 
@@ -89,14 +89,14 @@ const year = time.getFullYear();
 const month = time.getMonth() + 1;
 const date = time.getDate();
 
-const err = document.querySelector('.err');
+const err = document.querySelector(".err");
 if (year === 2024 && month === 6 && date === 7) {
-  err.style.display = 'flex';
+  err.style.display = "flex";
 }
 
-err.addEventListener('click', () => {
-  const element = document.createElement('form');
-  element.classList.add('user-check');
+err.addEventListener("click", () => {
+  const element = document.createElement("form");
+  element.classList.add("user-check");
   element.innerHTML = `
 		<div class="input-box">
 			<input type="text">
@@ -106,18 +106,18 @@ err.addEventListener('click', () => {
 
   document.body.appendChild(element);
 
-  window.addEventListener('click', (e) => {
+  window.addEventListener("click", (e) => {
     if (e.target === element) {
       document.body.removeChild(element);
     }
   });
 
   // PASS CHECK
-  element.addEventListener('submit', (e) => {
+  element.addEventListener("submit", (e) => {
     e.preventDefault();
-    const passValue = element.querySelector('input').value.toLowerCase();
-    if (passValue === '020422') {
-      window.location.href = './projects/bubbie.html';
+    const passValue = element.querySelector("input").value.toLowerCase();
+    if (passValue === "020422") {
+      window.location.href = "./projects/bubbie.html";
     } else {
       document.body.removeChild(element);
     }
@@ -125,13 +125,13 @@ err.addEventListener('click', () => {
 });
 
 // CONTACT FORM
-const clearBtn = document.querySelectorAll('.clear-btn');
+const clearBtn = document.querySelectorAll(".clear-btn");
 clearBtn.forEach((btn) => {
-  btn.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
     const input = btn.previousElementSibling;
 
-    if (input.value !== '') {
-      input.value = '';
+    if (input.value !== "") {
+      input.value = "";
       input.focus();
     } else {
       input.focus();
@@ -139,22 +139,22 @@ clearBtn.forEach((btn) => {
   });
 });
 
-const form = document.querySelector('.contact-form');
-form.addEventListener('submit', (e) => {
+const form = document.querySelector(".contact-form");
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const username = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const phone = document.getElementById('phone').value;
-  const msg = document.getElementById('msg').value;
+  const username = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+  const msg = document.getElementById("msg").value;
 
   console.log(username, email, phone, msg);
 
-  const url = `https://wa.me/+6287825000868?text=Name: ${username}%0aPhone: ${phone}%0aEmail: ${email}%0a%0a${msg}`;
-  window.open(url, '_blank').focus();
+  const url = `https://wa.me/+6287843902885?text=Name: ${username}%0aPhone: ${phone}%0aEmail: ${email}%0a%0a${msg}`;
+  window.open(url, "_blank").focus();
 
-  username.value = '';
-  email.value = '';
-  phone.value = '';
-  msg.value = '';
+  username.value = "";
+  email.value = "";
+  phone.value = "";
+  msg.value = "";
 });
